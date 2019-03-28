3a44c09706329b98cfecea8f7b1a9c05cc3fecb2
Roei Bracha <roei635@gmail.com>
2019-03-28 09:59:23 +0200

    Updates

---
+++ precache-manifest.2e453971c8c2a776399619c79b4d8b72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1151abacbe3592aee307",
+    "url": "/hackton0319/static/js/runtime~main.003b4034.js"
+  },
+  {
+    "revision": "1383b8b6ba26fd925a24",
+    "url": "/hackton0319/static/js/main.47521ef2.chunk.js"
+  },
+  {
+    "revision": "45ad97d8318cd20bf7a4",
+    "url": "/hackton0319/static/js/2.ec7821dd.chunk.js"
+  },
+  {
+    "revision": "1383b8b6ba26fd925a24",
+    "url": "/hackton0319/static/css/main.84a56698.chunk.css"
+  },
+  {
+    "revision": "84fdd7e31ce9f6150ceae0cf50b8cda8",
+    "url": "/hackton0319/index.html"
+  }
+];
