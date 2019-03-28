076f82d3a1a44925b7bf8cc7d5472d9142fea85c
eladto <eladtomer17@gmail.com>
2019-03-28 02:15:02 +0200

    all paper

--- client-side/src/Components/CoursePreview/CoursePreview.jsx
+++ client-side/src/Components/CoursePreview/CoursePreview.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Button from '@material-ui/core/Button';
+import {School} from "@material-ui/icons"
+
 import moment from 'moment'
 
+import {theme} from '../../config'
+
 import "./coursePreview.scss";
-import { Category } from '@material-ui/icons';
 
 class CoursePreview extends Component {
     constructor (props) {
@@ -16,7 +20,10 @@ class CoursePreview extends Component {
       }
       
     }
-    componentDidMount
+    joinToCourse = (e)=>{
+      e.stopPropagation();
+      console.log(e)
+    }
     clickOpen = ()=>this.setState((preState)=>({isOpen:!preState.isOpen})) 
     render() {
       const {isOpen} = this.state
@@ -33,35 +40,53 @@ class CoursePreview extends Component {
         max_participants,
         min_participants,
         category,
+        name,
         location,
         student,
         previous_knowledge} = this.props.course
       return (
           <Paper className='coursePaper' onClick={this.clickOpen}>
               <div className='coursePreview'>
-                <div className='courseHeader'>
+                <div className='courseHeader' style={{backgroundColor:theme.palette.primary[200]}}>
                   <div className='coursePhotoWrap'>
                     <img src={photo_url} className='coursePhoto'/>
                   </div>
-                  {isOpen ? <ExpandLessIcon className='openIcon' /> :  <ExpandMoreIcon  className='openIcon' /> }                 
+                  {isOpen ? <ExpandLessIcon className='openIcon'  /> :  <ExpandMoreIcon  className='openIcon'  /> }                 
                 </div>
                 <div className='courseLabel'>
-                    <span className='courseName'>{`קורס פייתון `}</span>
+                    <span className='courseName'>{`${name} (${category})`}</span>
                   </div>
                 <div className='row'>
-                  <span>תאריכים:</span><span>{`${moment(start_date).format('DD/MM/YYYY')}-${moment(end_date).format('DD/MM/YYYY')}`}</span>
+                  <span className='rowLabel'>תאריכים:</span><span>{`${moment(start_date).format('DD/MM/YYYY')}-${moment(end_date).format('DD/MM/YYYY')}`}</span>
                 </div>
                 <div className='row'>
-                  <span>מיקום:</span><span>{location.name}</span>
+                  <span className='rowLabel'>מיקום:</span><span>{location.name}</span>
                 </div>
                 <div className='row'>
-                  <span>מורה:</span><span>{`${student.userByUsername.first_name} ${student.userByUsername.last_name}`}</span>
+                  <span className='rowLabel'>מורה:</span><span>{`${student.userByUsername.first_name} ${student.userByUsername.last_name}`}</span>
                 </div>
-                {isOpen ? <div className='row'>
-                  <span>תיאור קורס:</span><p>{description}</p> 
-                </div>: null}
-                <div className='row'>
-                </div>
+                {isOpen ? 
+                <div>
+
+                  <div className='row'>
+                    <span className='rowLabel'>תיאור קורס:</span><span>{description}</span> 
+                  </div>
+                  <div className='row'>
+                    <span className='rowLabel'>שעות:</span><span>{`${start_hour}-${end_hour}`}</span> 
+                  </div>
+                    <div className='row'>
+                  <span className='rowLabel'>ידע קודם דרוש:</span><span>{previous_knowledge}</span> 
+                  </div>
+                  <div className='footer'>
+                  <Button variant="contained" color="primary" className='joinButton' onClick={this.joinToCourse}>
+                    <span> הצטרף לקורס</span> 
+                    <School style={{marginRight:'8px'}} />
+                  </Button>
+                  </div>
+                  <div className='row'>
+                  </div>
+                </div> :null
+                  }
               </div>
             </Paper>
             
