715d682766e077b0422a25cadc971332f2539731
Roei Bracha <roei635@gmail.com>
2019-03-28 09:52:12 +0200

    build

---
+++ client-side/build/precache-manifest.b902812d1f81603ad4b168cc39081936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "82f0b767896825c7c3dc",
+    "url": "/static/js/main.c3e06fb4.chunk.js"
+  },
+  {
+    "revision": "45589578ce9939b0111c",
+    "url": "/static/js/2.93f4cd2c.chunk.js"
+  },
+  {
+    "revision": "82f0b767896825c7c3dc",
+    "url": "/static/css/main.fa4c0f7c.chunk.css"
+  },
+  {
+    "revision": "776af7159ae5d8343fb543523b3359f4",
+    "url": "/index.html"
+  }
+];
